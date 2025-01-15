fbbefbc2319b59b98f7da49dc48c8b9d71840a18
elliotdotsol <hello@kite.studio>
2025-01-15 17:50:29 +0400

    feat: manifest

---
+++ app/manifest.ts
@@ -0,0 +1,45 @@
+import { MetadataRoute } from "next";
+
+export default function manifest(): MetadataRoute.Manifest {
+	return {
+		name: "Monad Tools",
+		short_name: "Monad Tools",
+		description: "Monad Tools is a community project made by Monad Pad. We are not affiliated with Monad.",
+		start_url: "/",
+		display: "standalone",
+		background_color: "#02040A",
+		theme_color: "#02040A",
+		icons: [
+			{
+				src: "/favicons/android-chrome-192x192.png",
+				sizes: "192x192",
+				type: "image/png",
+			},
+			{
+				src: "/favicons/android-chrome-512x512.png",
+				sizes: "512x512",
+				type: "image/png",
+			},
+			{
+				src: "/favicons/apple-touch-icon.png",
+				sizes: "180x180",
+				type: "image/png",
+			},
+			{
+				src: "/favicons/favicon-16x16.png",
+				sizes: "16x16",
+				type: "image/png",
+			},
+			{
+				src: "/favicons/favicon-32x32.png",
+				sizes: "32x32",
+				type: "image/png",
+			},
+			{
+				src: "/favicons/favicon.ico",
+				sizes: "64x64 32x32 24x24 16x16",
+				type: "image/x-icon",
+			},
+		],
+	};
+}
