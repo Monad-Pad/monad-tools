f8d0f4e0e97ebe07fefe5b8817c99bdd23981eba
elliotdotsol <hello@kite.studio>
2025-01-15 16:39:34 +0400

    feat: contract address in mint page

--- app/mint/page.tsx
+++ app/mint/page.tsx
@@ -63,6 +63,7 @@ export default async function MintPage() {
 												>
 													{formatAddress(c.creator_address)}
 												</Link>
+												.
 											</p>
 										</div>
 									</div>
--- components/tools/open-editions/mint-ui.tsx
+++ components/tools/open-editions/mint-ui.tsx
@@ -16,7 +16,7 @@ export function MintUI({ collectionAddress, data, collection }: { collectionAddr
 	const { getMintedTokens } = useOpenEditions();
 	const [mintedTokens, setMintedTokens] = useState(0);
 	const [isMinting, setIsMinting] = useState(false);
-    const [isNotPastEndTime, setIsNotPastEndTime] = useState(false);
+	const [isNotPastEndTime, setIsNotPastEndTime] = useState(false);
 
 	useEffect(() => {
 		async function fetchMintedTokens() {
@@ -36,12 +36,11 @@ export function MintUI({ collectionAddress, data, collection }: { collectionAddr
 		} else {
 			setIsMinting(false);
 		}
-        if (endDate > now) {
-            setIsNotPastEndTime(true);
-        } else {
-            setIsNotPastEndTime(false);
-        }
-
+		if (endDate > now) {
+			setIsNotPastEndTime(true);
+		} else {
+			setIsNotPastEndTime(false);
+		}
 	}, [collection.starts_at, collection.ends_at]);
 
 	return (
@@ -62,6 +61,11 @@ export function MintUI({ collectionAddress, data, collection }: { collectionAddr
 						>
 							{formatAddress(collection.creator_address)}
 						</Link>
+						.{" "}
+						<span className="text-muted-foreground font-medium">Address:</span>{" "}
+						<Link href={`https://explorer.monad-devnet.devnet101.com/address/${collection.contract_address}`} target="_blank" className="text-primary underline">
+							{formatAddress(collection.contract_address)}
+						</Link>
 					</p>
 				</div>
 				<div className="flex flex-col gap-2 mt-6">
@@ -75,9 +79,7 @@ export function MintUI({ collectionAddress, data, collection }: { collectionAddr
 					</div>
 					{!isMinting && isNotPastEndTime && (
 						<div className="w-full lg:w-1/2 mt-4">
-							<p className="text-sm uppercase text-muted-foreground font-semibold mb-1">
-								{isMinting ? "Minting ends in" : "Minting starts in"}
-							</p>
+							<p className="text-sm uppercase text-muted-foreground font-semibold mb-1">{isMinting ? "Minting ends in" : "Minting starts in"}</p>
 							<Countdown endsAt={isMinting ? collection.ends_at : collection.starts_at} size="full" />
 						</div>
 					)}
@@ -89,7 +91,14 @@ export function MintUI({ collectionAddress, data, collection }: { collectionAddr
 				</div>
 			</div>
 			<div className="flex flex-col items-center justify-center">
-				<Image unoptimized src={convertIpfsUrl(data.image)} alt={data.name} width={512} height={512} className="w-full aspect-square h-full object-cover rounded-2xl" />
+				<Image
+					unoptimized
+					src={convertIpfsUrl(data.image)}
+					alt={data.name}
+					width={512}
+					height={512}
+					className="w-full aspect-square h-full object-cover rounded-2xl"
+				/>
 			</div>
 		</>
 	);
